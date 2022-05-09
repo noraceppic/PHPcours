724fec707bfc3377861f41497b25f0e8d2afc55d
noraceppic <messanora86@gmail.com>
2022-05-09 16:59:00 +0200

    tableau

--- 02-variables/index.php
+++ 02-variables/index.php
@@ -25,8 +25,29 @@ $b=7;
 //  $a/=$b;
 // $a =$a % $b;
 // echo round ($a,3);
+/* 
+$PHT = 172 ;
+$TVA = 20 ;
+$PTT = $PHT+($PHT*$TVA /100) ;
+echo'le prix en HT est'. $PHT. '€'.'<br>';
+echo 'TVA= '. $TVA . ' % '.'<br>';
+echo 'le prix en TTC est ' .$PTT. '€'; */
+//calculer l'age d'une personne en renseigant son année de naissance ,
+// utiliser la fonction date
 
-$PHT =50
-$TVA
+$anneDeNaissance= 1990;
+$anneEnCours=date("Y");
+
+$age = $anneEnCours - $anneDeNaissance;
+echo 'votre age est '. $age .' ans <br>';
+//incrementation /decrementation 
+$i=0;
+$i +=1;
+$i++;
+/* 
+$j =10;
+si j=-=1;
+echo $j;
+
 ?>
-
+ */
---
+++ 03-tableaux/index.php
@@ -0,0 +1,17 @@
+<?php 
+//tableau indexé
+//declaration  d'un tableau 
+echo '<pre>';
+// $tableau = array();
+// $tableau = [];
+// $tableau = array ('Lundi','Mardi',45;67.8,false);
+// $tableau = [true,45,'terre' ,56,[45,67,78]];
+// print_r($tableau);
+
+
+$notes = [12,5,16,7];
+$moyenne = ($notes[0]+$notes[1]+$notes[2]+$notes[3])/4;
+echo $moyenne;
+echo'</pre>';
+?>
+
