229808065a05277295b648d5d7def34e5461f3bc
noraceppic <messanora86@gmail.com>
2022-05-09 11:30:36 +0200

    declaration du vraiable

--- 01-syntaxe/index.php
+++ 01-syntaxe/index.php
@@ -1,7 +1,46 @@
 <h1>un titre</h1>
 <?php
 // echo/print
-echo 'Texte en php'   ' Autre texte <br>';
+// echo 'Texte en php'   ' Autre texte <br>';
 // print 'Texte en php avec print<br>' ,Autre texte avec print;
+
+$variable='une variable';
+$variable='une autre variable';
+
+// $3var=34;
+$_3var=35;
+echo $_3var;
+$camlCase='une variable en camle case';
+$snack_case = 'en variable en snake case';
+$pascaleCase ='une variable en pascale case';
+$var = 'une variable en  kebab-case';
+
+// string
+$nom ='mohamed';
+$nombre=34;
+//int 
+$nombre=34;
+//  float
+$nobreDecimale=23.7;
+//bool
+$vrai= true;
+$faux=false;
+
+//array/objet
+$tableau =['groug',34,45,6,false];
+//null
+$variable =null;
+// imple/double  guillemets (quates)
+// $message ="aujourd'huit";
+$message ='aujourd\'huit';
+$txt ='bonjour';
+echo '$txt bonjour tout le monde <br>';
+echo "$txt bonjour tout le monde <br>";
+
+//concatination
+$fruit1='kiwi';
+$fruit1='pomme';
+$fruit2='banane';
+echo $fruit1 . <br> . $fruit2;
 ?>
 
